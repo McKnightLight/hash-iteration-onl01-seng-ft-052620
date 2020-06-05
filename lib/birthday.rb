b8de8d6ff2472a879d978df5fa9d932fe6d2e096
Jonathan McKnight <themcknightrider@gmail.com>
2020-06-05 14:29:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,10 +12,9 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   winner = ""
-  birthday_kids.each do |age, name|
-    if age >  && name.start_with?("A")
-      winner = name
+  birthday_kids.each do |kids_name, age|
+    if age > 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
     end
   end
-winner
 end
